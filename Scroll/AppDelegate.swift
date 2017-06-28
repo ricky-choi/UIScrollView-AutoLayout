a5ba7484a793c94378c78743622306f65c40c0a8
Jae Young Choi <hideyf@gmail.com>
2017-06-29 02:28:49 +0900

    first commit

---
+++ Scroll/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Scroll
+//
+//  Created by Jae Young Choi on 2017. 6. 28..
+//  Copyright © 2017년 Appcid. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
